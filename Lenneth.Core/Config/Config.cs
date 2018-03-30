d8db90e9b442b61e1596652f2d9b8b683797c70a
zyltntking <zyltntking@qq.com>
2018-03-30 17:09:38 +0800

    Lenneth

---
+++ Lenneth.Core/Config/Config.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Lenneth.Core
+{
+    internal static class Config
+    {
+
+        #region Nlog配置
+
+        public static LoggingConfiguration LogConfig
+        {
+            get
+            {
+                var config = new LoggingConfiguration();
+
+                var consoleTarget = new ColoredConsoleTarget();
+                config.AddTarget("console", consoleTarget);
+                consoleTarget.Layout = @"${date:format=HH\:mm\:ss} ${logger} ${message}";
+                config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, consoleTarget));
+
+                var fileTarget = new FileTarget();
+                config.AddTarget("file", fileTarget);
+                fileTarget.FileName = @"${basedir}/log/" + $"{DateTime.Now:yyyy-M-d}.txt";
+                fileTarget.Layout = @"${date:format=yyyy-MM-dd HH\:mm\:ss.fff}  |${logger}  |${level:format=Name}  |${message}  |${exception:format=toString,Data}";
+                config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, fileTarget));
+
+                return config;
+            }
+        }
+
+        #endregion
+    }
+}
