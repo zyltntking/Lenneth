78520195042250420379c4195867d3651f57e3c6
zyltntking <zyltntking@qq.com>
2018-04-02 14:11:44 +0800

    Lenneth

---
+++ Lenneth.Core/Framework/FastDFS/Common/FdfsResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Lenneth.Core.Framework.FastDFS.Common
+{
+
+    public class FdfsResponse
+    {
+        public virtual void ReceiveResponse(Stream stream, long length)
+        {
+            var content = new byte[length];
+            stream.Read(content, 0, (int) length);
+            LoadContent(content);
+        }
+
+        protected virtual void LoadContent(byte[] content)
+        {
+        }
+    }
+}
---
+++ Lenneth.Core/Framework/FastDFS/Config/FastDFSConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Lenneth.Core.Framework.FastDFS.Config
+{
+    public abstract class FastDfsConfig
+    {
+        protected FastDfsConfig()
+        {
+            FastDfsServer = new List<FastDfsServer>();
+        }
+
+        public string GroupName { get; set; }
+
+        public List<FastDfsServer> FastDfsServer { get; set; }
+    }
+}
