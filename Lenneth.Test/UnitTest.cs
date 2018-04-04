17eaebc8ba1cb30af7e81bad73b577e1911a8f85
zyltntking <zyltntking@qq.com>
2018-04-04 11:13:12 +0800

    Lenneth

---
+++ Lenneth.Test/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿using Lenneth.Core;
+using Xunit;
+
+namespace Lenneth.Test
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void HashTest()
+        {
+            var str = Facade.Hash.ComputeHash("12345678");
+            Assert.Equal("25d55ad283aa400af464c76d713c07ad", str);
+        }
+    }
+}
