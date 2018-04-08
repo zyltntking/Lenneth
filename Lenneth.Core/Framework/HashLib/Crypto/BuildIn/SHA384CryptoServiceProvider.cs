1d7a5e156fb44b8b8c9d07a592436a950c6e3de4
赵YN <zyltntking@live.cn>
2018-04-08 13:46:56 +0800

    Lenneth

---
+++ Lenneth.Core/Framework/HashLib/Crypto/BuildIn/SHA384CryptoServiceProvider.cs
@@ -0,0 +1,15 @@
+﻿#if !NETCORE
+
+using System;
+
+namespace Lenneth.Core.Framework.HashLib.Crypto.BuildIn
+{
+    internal class SHA384CryptoServiceProvider : HashCryptoBuildIn
+    {
+        public SHA384CryptoServiceProvider() 
+            : base(new System.Security.Cryptography.SHA384CryptoServiceProvider(), 128)
+        {
+        }
+    }
+}
+#endif
