85ed9ae58e6de71d55da2a50d3da60fc1386b2e8
zyltntking <zyltntking@qq.com>
2018-04-18 16:29:40 +0800

    Lenneth

---
+++ Lenneth.Core/Extensions/Caching/Extensions.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Runtime.Caching;
+
+namespace Lenneth.Core.Extensions.Caching
+{
+    public static class Extensions
+    {
+        #region FromCache
+
+        /// <summary>A TKey extension method that from cache.</summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="cache">The cache.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue FromCache<T, TValue>(this T @this, MemoryCache cache, string key, TValue value)
+        {
+            var item = cache.AddOrGetExisting(key, value, new CacheItemPolicy()) ?? value;
+
+            return (TValue)item;
+        }
+
+        /// <summary>A TKey extension method that from cache.</summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue FromCache<T, TValue>(this T @this, string key, TValue value)
+        {
+            return @this.FromCache(MemoryCache.Default, key, value);
+        }
+
+        /// <summary>A TKey extension method that from cache.</summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="cache">The cache.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="valueFactory">The value factory.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue FromCache<T, TValue>(this T @this, MemoryCache cache, string key, Expression<Func<T, TValue>> valueFactory)
+        {
+            var lazy = new Lazy<TValue>(() => valueFactory.Compile()(@this));
+            var item = (Lazy<TValue>)cache.AddOrGetExisting(key, lazy, new CacheItemPolicy()) ?? lazy;
+            return item.Value;
+        }
+
+        /// <summary>A TKey extension method that from cache.</summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="valueFactory">The value factory.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue FromCache<T, TValue>(this T @this, string key, Expression<Func<T, TValue>> valueFactory)
+        {
+            return @this.FromCache(MemoryCache.Default, key, valueFactory);
+        }
+
+        /// <summary>A TKey extension method that from cache.</summary>
+        /// <typeparam name="TKey">Type of the key.</typeparam>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="valueFactory">The value factory.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue FromCache<TKey, TValue>(this TKey @this, Expression<Func<TKey, TValue>> valueFactory)
+        {
+            var key = string.Concat("Z.Caching.FromCache;", typeof(TKey).FullName, valueFactory.ToString());
+            return @this.FromCache(MemoryCache.Default, key, valueFactory);
+        }
+
+        /// <summary>A TKey extension method that from cache.</summary>
+        /// <typeparam name="TKey">Type of the key.</typeparam>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="cache">The cache.</param>
+        /// <param name="valueFactory">The value factory.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue FromCache<TKey, TValue>(this TKey @this, MemoryCache cache, Expression<Func<TKey, TValue>> valueFactory)
+        {
+            var key = string.Concat("Z.Caching.FromCache;", typeof(TKey).FullName, valueFactory.ToString());
+            return @this.FromCache(cache, key, valueFactory);
+        }
+
+        #endregion FromCache
+
+        #region AddOrGetExisting
+
+        /// <summary>A MemoryCache extension method that adds an or get existing.</summary>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="cache">The cache to act on.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue AddOrGetExisting<TValue>(this MemoryCache cache, string key, TValue value)
+        {
+            var item = cache.AddOrGetExisting(key, value, new CacheItemPolicy()) ?? value;
+
+            return (TValue)item;
+        }
+
+        /// <summary>A MemoryCache extension method that adds an or get existing.</summary>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="cache">The cache to act on.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="valueFactory">The value factory.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue AddOrGetExisting<TValue>(this MemoryCache cache, string key, Func<string, TValue> valueFactory)
+        {
+            var lazy = new Lazy<TValue>(() => valueFactory(key));
+
+            var item = (Lazy<TValue>)cache.AddOrGetExisting(key, lazy, new CacheItemPolicy()) ?? lazy;
+
+            return item.Value;
+        }
+
+        /// <summary>A MemoryCache extension method that adds an or get existing.</summary>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="cache">The cache to act on.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="valueFactory">The value factory.</param>
+        /// <param name="policy">The policy.</param>
+        /// <param name="regionName">(Optional) name of the region.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue AddOrGetExisting<TValue>(this MemoryCache cache, string key, Func<string, TValue> valueFactory, CacheItemPolicy policy, string regionName = null)
+        {
+            var lazy = new Lazy<TValue>(() => valueFactory(key));
+
+            var item = (Lazy<TValue>)cache.AddOrGetExisting(key, lazy, policy, regionName) ?? lazy;
+
+            return item.Value;
+        }
+
+        /// <summary>A MemoryCache extension method that adds an or get existing.</summary>
+        /// <typeparam name="TValue">Type of the value.</typeparam>
+        /// <param name="cache">The cache to act on.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="valueFactory">The value factory.</param>
+        /// <param name="absoluteExpiration">The policy.</param>
+        /// <param name="regionName">(Optional) name of the region.</param>
+        /// <returns>A TValue.</returns>
+        public static TValue AddOrGetExisting<TValue>(this MemoryCache cache, string key, Func<string, TValue> valueFactory, DateTimeOffset absoluteExpiration, string regionName = null)
+        {
+            var lazy = new Lazy<TValue>(() => valueFactory(key));
+
+            var item = (Lazy<TValue>)cache.AddOrGetExisting(key, lazy, absoluteExpiration, regionName) ?? lazy;
+
+            return item.Value;
+        }
+
+        #endregion AddOrGetExisting
+    }
+}
---
+++ Lenneth.Core/Extensions/Collections/Extensions.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lenneth.Core.Extensions.Collections
+{
+    public static class Extensions
+    {
+        #region ICollection AddIf
+
+        /// <summary>
+        ///     An ICollection&lt;T&gt; extension method that adds only if the value satisfies the predicate.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>true if it succeeds, false if it fails.</returns>
+        public static bool AddIf<T>(this ICollection<T> @this, Func<T, bool> predicate, T value)
+        {
+            if (predicate(value))
+            {
+                @this.Add(value);
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion ICollection AddIf
+
+        #region ICollection AddIfNotContains
+
+        /// <summary>
+        ///     An ICollection&lt;T&gt; extension method that add value if the ICollection doesn't contains it already.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="value">The value.</param>
+        /// <returns>true if it succeeds, false if it fails.</returns>
+        public static bool AddIfNotContains<T>(this ICollection<T> @this, T value)
+        {
+            if (!@this.Contains(value))
+            {
+                @this.Add(value);
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion ICollection AddIfNotContains
+
+        #region ICollection AddRange
+
+        /// <summary>
+        ///     An ICollection&lt;T&gt; extension method that adds a range to 'values'.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        public static void AddRange<T>(this ICollection<T> @this, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                @this.Add(value);
+            }
+        }
+
+        #endregion ICollection AddRange
+
+        #region ICollection AddRangeIf
+
+        /// <summary>
+        ///     An ICollection&lt;T&gt; extension method that adds a collection of objects to the end of this collection only
+        ///     for value who satisfies the predicate.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        public static void AddRangeIf<T>(this ICollection<T> @this, Func<T, bool> predicate, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (predicate(value))
+                {
+                    @this.Add(value);
+                }
+            }
+        }
+
+        #endregion ICollection AddRangeIf
+
+        #region ICollection AddRangeIfNotContains
+
+        /// <summary>
+        ///     An ICollection&lt;T&gt; extension method that adds a range of values that's not already in the ICollection.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        public static void AddRangeIfNotContains<T>(this ICollection<T> @this, params T[] values)
+        {
+            foreach (var value in values)
+            {
+                if (!@this.Contains(value))
+                {
+                    @this.Add(value);
+                }
+            }
+        }
+
+        #endregion ICollection AddRangeIfNotContains
+
+        #region ICollection ContainsAll
+
+        /// <summary>
+        ///     An ICollection&lt;T&gt; extension method that query if '@this' contains all values.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        /// <returns>true if it succeeds, false if it fails.</returns>
+        public static bool ContainsAll<T>(this ICollection<T> @this, params T[] values)
+        {
+            return values.All(@this.Contains);
+        }
+
+        #endregion ICollection ContainsAll
+
+        #region ICollection ContainsAny
+
+        /// <summary>
+        ///     An ICollection&lt;T&gt; extension method that query if '@this' contains any value.
+        /// </summary>
+        /// <typeparam name="T">Generic type parameter.</typeparam>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="values">A variable-length parameters list containing values.</param>
+        /// <returns>true if it succeeds, false if it fails.</returns>
+        public static bool ContainsAny<T>(this ICollection<T> @this, params T[] values)
+        {
+            foreach (T value in values)
+            {
+                if (@this.Contains(value))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        #endregion ICollection ContainsAny
+    }
+}
