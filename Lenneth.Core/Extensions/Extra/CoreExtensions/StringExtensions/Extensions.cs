0079bd5808292fb0ee10c10460004084a0392e39
zyltntking <zyltntking@qq.com>
2018-04-23 13:05:32 +0800

    Lenneth

--- Lenneth.Core/Extensions/Extra/CoreExtensions/StringExtensions/Extensions.cs
+++ Lenneth.Core/Extensions/Extra/CoreExtensions/StringExtensions/Extensions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Data;
+using System.Data.Entity.Design.PluralizationServices;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -2567,26 +2568,26 @@ namespace Lenneth.Core.Extensions.Extra.CoreExtensions.StringExtensions
 
         #region ToPlural
 
-        ///// <summary>
-        /////     A string extension method that converts the @this to a plural.
-        ///// </summary>
-        ///// <param name="this">The @this to act on.</param>
-        ///// <returns>@this as a string.</returns>
-        //public static string ToPlural(this string @this)
-        //{
-        //    return PluralizationService.CreateService(new CultureInfo("en-US")).Pluralize(@this);
-        //}
+        /// <summary>
+        ///     A string extension method that converts the @this to a plural.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <returns>@this as a string.</returns>
+        public static string ToPlural(this string @this)
+        {
+            return PluralizationService.CreateService(new CultureInfo("en-US")).Pluralize(@this);
+        }
 
-        ///// <summary>
-        /////     A string extension method that converts the @this to a plural.
-        ///// </summary>
-        ///// <param name="this">The @this to act on.</param>
-        ///// <param name="cultureInfo">Information describing the culture.</param>
-        ///// <returns>@this as a string.</returns>
-        //public static string ToPlural(this string @this, CultureInfo cultureInfo)
-        //{
-        //    return PluralizationService.CreateService(cultureInfo).Pluralize(@this);
-        //}
+        /// <summary>
+        ///     A string extension method that converts the @this to a plural.
+        /// </summary>
+        /// <param name="this">The @this to act on.</param>
+        /// <param name="cultureInfo">Information describing the culture.</param>
+        /// <returns>@this as a string.</returns>
+        public static string ToPlural(this string @this, CultureInfo cultureInfo)
+        {
+            return PluralizationService.CreateService(cultureInfo).Pluralize(@this);
+        }
 
         #endregion ToPlural
 
