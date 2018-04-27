250de843a9957448a4cc8fa5421df8b68cc049b9
zyltntking <zyltntking@qq.com>
2018-04-27 10:12:08 +0800

    Lenneth

--- Lenneth.WebApi/Core/Filter/ExceptionHandler.cs
+++ Lenneth.WebApi/Core/Filter/ExceptionHandler.cs
@@ -1,5 +1,9 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
 using Lenneth.WebApi.Core.Log;
@@ -11,17 +15,30 @@ namespace Lenneth.WebApi.Core.Filter
     /// <summary>
     /// WebApi 异常处理程序
     /// </summary>
-    internal class ExceptionHandler : ExceptionFilterAttribute
+    internal class ExceptionHandler : FilterAttribute, IExceptionFilter
     {
-        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        //public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        //{
+        //    ILogWapper log = new NLogWrapper(AppConfig.WebApiExceptionLogConfig);
+        //    log.Error(actionExecutedContext.Exception, actionExecutedContext.Request.RequestUri.LocalPath);
+
+        //    var apiException = ResponseUtility.InitResult<object>(MessageCode.ApiInterfaceException);
+        //    apiException.Data = actionExecutedContext.Exception.Message;
+
+        //    actionExecutedContext.Response = actionExecutedContext.Request.CreateResponse(HttpStatusCode.InternalServerError, apiException);
+        //}
+
+        #region Implementation of IExceptionFilter
+
+        /// <summary>执行异步异常筛选器。</summary>
+        /// <returns>异步异常筛选器。</returns>
+        /// <param name="actionExecutedContext">操作执行的上下文。</param>
+        /// <param name="cancellationToken">取消标记。</param>
+        public Task ExecuteExceptionFilterAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
         {
-            ILogWapper log = new NLogWrapper(AppConfig.WebApiExceptionLogConfig);
-            log.Error(actionExecutedContext.Exception, actionExecutedContext.Request.RequestUri.LocalPath);
-
-            var apiException = ResponseUtility.InitResult<object>(MessageCode.ApiInterfaceException);
-            apiException.Data = actionExecutedContext.Exception.Message;
-
-            actionExecutedContext.Response = actionExecutedContext.Request.CreateResponse(HttpStatusCode.InternalServerError, apiException);
+            throw new NotImplementedException();
         }
+
+        #endregion
     }
 }
---
+++ Lenneth.WebApi/Core/Filter/ResponseCrypt.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Http.Filters;
+
+namespace Lenneth.WebApi.Core.Filter
+{
+    /// <summary>
+    /// 响应加密处理程序
+    /// </summary>
+    internal class ResponseCrypt : ActionFilterAttribute
+    {
+    }
+}
